5908394ae34cc12f98010932379ab5bfd8b6588a
abdalkader_omar <jojo78825@gmail.com>
2026-05-10 10:13:16 +0300

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -1,39 +1,44 @@
-#caesar cipher 
-alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-should_continue=True
-def encrypt():
-        cipher=""
-        for letter in text:
-            if letter in alphabet:
-                postion= alphabet.index(letter)
-                new_postion= (postion+shift) %26
-                final=alphabet[new_postion]
-                cipher+=final
-        print(f"your encoded results is {cipher}")
-def decrypt ():
-        cipher_decrypt=""
-        for letter in text:
-            if letter in alphabet:
-                postion=alphabet.index(letter)
-                new_pos=(postion - shift) %26
-                final=alphabet[new_pos]
-                cipher_decrypt+=final
-        print(f"your decoded results is {cipher_decrypt}")            
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(plain_text, shift_amount):
+    cipher = ""
+    for letter in plain_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = (position + shift_amount) % 26
+            cipher += alphabet[new_position]
+        else:
+            cipher += letter
+    print(f"Your encoded result is: {cipher}")
+
+def decrypt(cipher_text, shift_amount):
+    decipher = ""
+    for letter in cipher_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = (position - shift_amount) % 26
+            decipher += alphabet[new_position]
+        else:
+            decipher += letter
+    print(f"Your decoded result is: {decipher}")
+
+should_continue = True
+
 while should_continue:
-    order=input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text=input("Type your message:\n").lower()
-    shift=int(input("Type the shift number:\n"))    
+    order = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
     if order == "encode":
-        encrypt()
+        encrypt(plain_text=text, shift_amount=shift)
     elif order == "decode":
-        decrypt()
+        decrypt(cipher_text=text, shift_amount=shift)
     else:
-        print("false input, pleas try again")  
-    result=input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
-    if result == "yes":
-        should_continue=True
-    elif result == "no":
-        should_continue= False
-        print("good buy!")
+        print("Invalid input, please try again.")
+
+    result = input("\nType 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if result == "no":
+        should_continue = False
+        print("Goodbye!")
 
 
