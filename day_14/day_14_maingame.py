76f102b2199c784c201362fd1e3963a312706229
abdalkader_omar <jojo78825@gmail.com>
2026-05-28 19:33:12 +0300

    .

---
+++ day_14/day_14_maingame.py
@@ -0,0 +1,55 @@
+#main game
+from game_data import data
+from game_data import logo
+from game_data import vs_art
+import random 
+import os
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+print(logo)
+def accounts():
+    goal=False
+    while not goal:
+        A=random.choice(data)
+        B=random.choice(data)
+        if A==B:
+            goal=False
+        else:
+            goal=True
+    # compare the followers count
+    comp=0
+    if A['follower_count'] > B['follower_count']:
+        comp+=1
+    else:
+        comp==0
+    return A, B, comp        
+
+def game(score):
+        print(f"Compare A: {acc1['name']}, a {acc1['description']}, from {acc1['country']}.\n{vs_art}\nCompare B: {acc2['name']}, a {acc2['description']}, from {acc2['country']}.")
+        user=input("Who has more followers? Type 'A' or 'B': ").lower()
+        if user=="a" and comp==1:
+            score+=1
+            clear_screen()
+            print(f"You're right! Current score: {score}.")
+        elif user=="b" and comp==0:
+            score+=1
+            clear_screen()
+            print(f"You're right! Current score: {score}.")
+        else:
+            clear_screen()
+            print(f"Sorry, that's wrong. Final score: {score}.")  
+            return -1
+        return score
+
+goal=False
+score1=0
+while not goal:
+    acc1, acc2, comp = accounts()
+    f_score=game(score=score1)
+    if f_score==-1:
+        goal=True
+    else:
+        score1=f_score    
+    
+
---
+++ day_14/game_data.py
@@ -0,0 +1,90 @@
+#game data 
+data = [
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 615,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Instagram',
+        'follower_count': 665,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 495,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 429,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 395,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'MrBeast',
+        'follower_count': 48,
+        'description': 'YouTuber and Philanthropist',
+        'country': 'United States'
+    },
+    {
+        'name': 'PlayStation',
+        'follower_count': 32,
+        'description': 'Gaming brand',
+        'country': 'Japan'
+    },
+    {
+        'name': 'Rockstar Games',
+        'follower_count': 28,
+        'description': 'Video game developer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Valorant',
+        'follower_count': 3,
+        'description': 'Video game',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 184,
+        'description': 'Actress and musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 97,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Eminem',
+        'follower_count': 40,
+        'description': 'Musician',
+        'country': 'United States'
+    }
+]
+logo = '''
+    __  ___       __               __                       
+   / / / (_)___ _/ /_  ___  ______/ /   ____ _      _____  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/ /   / __ \ | /| / / _ \/ ___/
+ / __  / / /_/ / / / /  __/ /  / /___/ /_/ / |/ |/ /  __/ /    
+/_/ /_/_/\__, /_/ /_/\___/_/  /_____/\____/|__/|__/\___/_/     
+        /____/                                                 
+'''
+vs_art = '''
+ _    __ _____
+| |  / // ___/
+| | / / \__ \ 
+| |/ / ___/ / 
+|___/ /____/  
+'''
