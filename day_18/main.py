6c75c27e8aa51135909316e10114b60dd0a74a73
abdalkader_omar <jojo78825@gmail.com>
2026-06-16 15:58:30 +0300

    Create main.py

---
+++ day_18/main.py
@@ -0,0 +1,23 @@
+# I extracted the color from an image i downloaded online.
+color_list=[(202, 164, 110),(149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+import random
+import turtle
+from turtle import Turtle , Screen
+turtle.colormode(255)
+azoz=Turtle()
+scr=Screen()
+azoz.penup()
+azoz.speed("fastest")
+def draw_dots():
+    x = -225
+    y = -225
+    for _ in range(10):
+        azoz.teleport( x ,y )
+        for _ in range(10):
+            color = random.choice(color_list)
+            azoz.dot(20 , color)
+            azoz.fd(50)
+        y+=50
+draw_dots()
+azoz.hideturtle()
+scr.exitonclick()
